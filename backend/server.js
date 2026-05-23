4395d4c9fdec7ed1d0d0b517979eee814cf219b1
Namit Malasi <namitmalasi@gmail.com>
2026-05-23 11:22:56 +0530

    feat: implement JWT authentication backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
